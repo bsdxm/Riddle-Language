4aa820937517e17c040d47bd1ff69bdebd74923f
墨川居士 <ethan_mochujushi@outlook.com>
2024-11-11 18:07:16 +0800

    Update RiddleParser.cpp

--- parser/RiddleParser.cpp
+++ parser/RiddleParser.cpp
@@ -1,5 +1,5 @@
 
-// Generated from C:/Users/wangz/CLionProjects/Riddle-Language/RiddleParser.g4 by ANTLR 4.13.1
+// Generated from Riddle-Language/RiddleParser.g4 by ANTLR 4.13.1
 
 
 #include "RiddleParserListener.h"
